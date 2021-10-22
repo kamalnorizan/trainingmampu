e6c661b906ae4896bff70e3b60a504ed067dc4fa
kamalnorizan <kamalnorizan@gmail.com>
2021-10-22 15:40:45 +0800

    functionArgument -p2

--- functionArgument.py
+++ functionArgument.py
@@ -1,10 +1,24 @@
-def printme(mystr, num=10):
+def printme(mystr, num=50):
     print(mystr+str(num))
     return
 
 printme('This is my first function call ', 20)
 printme('This is my second function call ', 15)
-printme(num = 20, mystr = 'This is my second function call ')
+printme('This is my third function call without num param ')
+printme(num = 20, mystr = 'This is my fourth function call using keword argument ')
+
+def printwithvarlength(arg1, *args):
+    print('Output for arg1 = '+str(arg1))
+
+    print(args)
+    for var in args:
+        print(var)
+
+    return
+
+
+# printwithvarlength(20)
+printwithvarlength(20,30,40,50,60)
 
 # 1) Required positional arguments
 # printme('This is my first function call ', 20)
@@ -14,3 +28,5 @@ printme(num = 20, mystr = 'This is my second function call ')
 
 # 3) Keyword arguments
 # printme(num = 20, mystr = 'This is my second function call ')
+
+# 4) Variable-length argument
