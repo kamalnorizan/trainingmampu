a4ae8ce1ccb53ac3dc7a909c3088d2bcb0cc8b26
kamalnorizan <kamalnorizan@gmail.com>
2021-10-22 09:33:57 +0800

    'whileLoop'

--- ifstatement.py
+++ ifstatement.py
@@ -5,4 +5,5 @@ if(var == 10) : print('value is 10')
 else : print('value is not 10')
 
 if(var2) : print('true')
+elif(var2==0) : print('zero')
 else : print('false')
---
+++ whileloop.py
@@ -0,0 +1,7 @@
+count = 0
+
+while (count < 9) :
+ print('The count is:'+str(count))
+ count+=1
+
+print('End loop')
