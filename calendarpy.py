b5c21a886fb68e24214d12abc4fb4147484f9745
kamalnorizan <kamalnorizan@gmail.com>
2021-10-22 11:57:39 +0800

    calendar-2

--- calendarpy.py
+++ calendarpy.py
@@ -3,5 +3,11 @@ import calendar
 cal = calendar.month(2021, 11)
 print(cal)
 
-yearcal = calendar.calendar(2021)
+yearcal = calendar.calendar(2022)
 print(yearcal)
+
+leapcheck = calendar.isleap(2024)
+print(leapcheck)
+
+leapday = calendar.leapdays(2000,2025)
+print(leapday)
---
+++ htmlcalendarmethod.py
@@ -0,0 +1,5 @@
+import calendar
+
+text_cal = calendar.HTMLCalendar()
+
+print(text_cal.formatyearpage(2021))
