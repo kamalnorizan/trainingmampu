e6c33c18a65b554bf4b5d12be5f53c32d6ef12d5
kamalnorizan <kamalnorizan@gmail.com>
2021-10-22 09:55:43 +0800

    forloop

---
+++ forloop.py
@@ -0,0 +1,11 @@
+# Forloop
+# Read each element in list
+# Iterator object
+
+list = [1,2,3,4,5,6,7,8]
+it = iter(list)
+
+for x in it:
+ print(x, end="\n")
+
+print('end forloop')
--- whileloop.py
+++ whileloop.py
@@ -1,5 +1,10 @@
 count = 0
 
+#this is a while loop example
+"""
+This is an example of block comment
+for multiline comments
+"""
 while (count < 9) :
  print('The count is:'+str(count))
  count+=1
