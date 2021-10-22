7b3a7264ed726ae00be3c1e21934951b6fc4a5b1
kamalnorizan <kamalnorizan@gmail.com>
2021-10-22 10:10:59 +0800

    usingnext

--- forloop.py
+++ forloop.py
@@ -2,11 +2,19 @@
 # Read each element in list
 # Iterator object
 # Menggunakan for loop
+import sys
 
 list = [1,2,3,4,5,6,7,8]
 it = iter(list)
-
+it2 = iter(list)
 for x in it:
- print(x, end="\n")
+ print(x, end='\n')
 
-print('end forloop')
+print('end "forloop"')
+
+# Menggunakan for next
+while True:
+ try:
+  print(next(it2))
+ except StopIteration:
+  sys.exit()
