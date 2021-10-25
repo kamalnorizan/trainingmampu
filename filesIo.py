65eeb9b1a7fabb89ecd336ab5cd344e90c4040cd
kamalnorizan <kamalnorizan@gmail.com>
2021-10-25 10:06:16 +0800

    fileIO p2

--- filesIo.py
+++ filesIo.py
@@ -1,2 +1,6 @@
-str = input('Please enter your age : ')
-print('Received input for age is : ',str)
+inputStr = input('Please enter your age : ')
+print('Received input for age is : ',inputStr)
+
+fo = open("myTxtfile.txt", 'a')
+fo.write('Test write in file your age '+str(inputStr)+'\n')
+fo.close()
