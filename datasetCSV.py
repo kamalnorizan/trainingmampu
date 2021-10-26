808f5c82d33f334a447d535efa41b0ee4cb65cd0
kamalnorizan <kamalnorizan@gmail.com>
2021-10-26 15:06:08 +0800

    'dfPandas'

--- datasetCSV.py
+++ datasetCSV.py
@@ -7,7 +7,7 @@
 
 
 
-# In[31]:
+# In[99]:
 
 
 import pandas as pd
@@ -56,11 +56,30 @@ table = pd.pivot_table(df, values='ID', index=['Marital_Status'], aggfunc='count
 print(table)
 
 
-# In[59]:
+# In[83]:
 
 
 tableSumofIncome = pd.pivot_table(df, values=['ID','Income'], index=['Education'], aggfunc={'ID':'count','Income':'sum'})
+tableSumofIncome['avg']=tableSumofIncome['Income']/tableSumofIncome['ID']
+tableSumofIncome['additional']=10
 print(tableSumofIncome)
+
+
+# In[106]:
+
+
+wines = sum(df['MntWines'])
+fruits = sum(df['MntFruits'])
+meat = sum(df['MntMeatProducts'])
+fish = sum(df['MntFishProducts'])
+sweet = sum(df['MntSweetProducts'])
+gold = sum(df['MntGoldProds'])
+
+data = {'wines': wines, 'fruits': fruits, 'meat':meat, 'fish': fish, 'sweet': sweet, 'gold': gold}
+s = pd.Series(data)
+df2 = pd.DataFrame(s)
+df2['percentage']=df2[0]/sum(s)*100
+print(df2)
 
 
 # In[ ]:
